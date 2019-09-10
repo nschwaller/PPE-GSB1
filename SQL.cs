0eb098d29b2f4b8bddcfd2d8fb93d4dee31086ef
rreiter <rreiter@S-204-16>
2019-09-10 16:59:07 +0200

    SQL FINI (enfin normalment ....)

--- SQL.cs
+++ SQL.cs
@@ -9,9 +9,13 @@ namespace PPE_GSB1
 {
     class SQL
     {
+        private string IP = "172.19.0.4";
+        private string USER = "phpmyadmin";
+        private string MDP = "0550002D";
+        private string DATABASE = "GRB";
         public SQL()
         {
-            string connStr = "server=172.19.0.4;user=phpmyadmin;database=GRB;port=3306;password=0550002D";
+            string connStr = "server=" + this.IP + ";user=" + this.USER + ";database=" + this.DATABASE + ";port=3306;password=" + this.MDP;
             MySqlConnection conn = new MySqlConnection(connStr);
         }
     }
